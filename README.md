1469effd06cfdbd1c5ad46d8f46a69509b4b5dd3
Sourabh Niyogi <sourabh@colorfulnotion.com>
2023-02-23 02:37:10 +0000

    new account + system schemas; chain/symbol/assets reporting
    * account schemas: accountsnew/reaped/active/passive
    * system schemas: assets/xcmassets/chains

--- README.md
+++ README.md
@@ -31,26 +31,7 @@ Then, set up a Google Cloud Platform (GCP) account to access the project/dataset
 
 ### Quick Start
 
-You can access Substrate ETL data  in one of two ways:
-1. Using the `substrateetl` CLI
-
-1. Using the GCP BigQuery console
-
-The `substrateetl` CLI may be preferred in command line scripts whereas the GCP BigQuery console may be preferred for developing custom exploratory queries.
-
-#### Using the `substrateetl` CLI
-
-_Get blocks and extrinsics of paraid 2007_ ([Schema](#blocksjson), [Reference](#export)):
-
-```bash
-# ./substrateetl export \
- --start-date 2022-12-25 --end-date 2022-12-31 \
- --r kusama -p 2007 \
- --blocks-output blocks.json \
- --extrinsics-output extrinsics.json 
-```
-
-This exports blocks and extrinsics of kusama paraID 2007 (shiden) to files "blocks.json" and "extrinsics.json".
+You can access Substrate ETL data using the GCP BigQuery console.
 
 _Get XCM transfers of polkadot_ ([Schema](#xcmtransfersjson), [Reference](#xcmtransfers)):
 
@@ -98,7 +79,46 @@ select * from `substrate-etl.polkadot.xcmtransfers` where DATE(block_time) >= "2
       - [xcmtransfers](#xcmtransfers)
     - [Running Tests](#running-tests)
 
-## Schema
+### Public Datasets in BigQuery
+
+Substrate data for each chain is held in 7 tables in one of 2 public
+datasets, with one dataset for each relay chain and all its
+parachains.  By convention, relaychain data is considered "paraid=0".
+
+Project: (Location: us-central1)
+* `substrate-etl`
+
+Datasets: 
+* `polkadot`
+* `kusama`
+
+Tables: (replace `{paraid}` with a specific paraid, e.g. `2000` for `acala`)
+* `blocks{paraid}`
+* `extrinsics{paraid}`
+* `transfers{paraid}`
+* `events{paraid}`
+* `logs{paraid}`
+* `accountsactive{paraid}`
+* `accountspassive{paraid}`
+* `accountsnew{paraid}`
+* `accountsreaped{paraid}`
+* `chains` (system)
+* `assets` (system)
+* `xcmassets` (system)
+* `xcmtransfers` (system)
+
+Thus polkadot relay chain blocks are held in `substrate-etl.polkadot.blocks0`, acala blocks are stored in `substrate-etl.polkadot.blocks2000`, and so on.  
+
+See [Definitions](/DEFINITIONS.md) for how the tables are constructed and tentative definitions.
+
+Every chains has a auto generated README with the chains tables explicitly enumerated, and includes sample queries.
+
+Notes:
+* System tables (`chains`, `assets`, `xcmassets`, `xcmtransfers`) are not specific to any parachain and apply to the whole relay chain.
+* All tables (except for `chains`, `assets` and `xcmassets`) are date-partitioned  to support low cost, high speed scans.
+* If a parachain has a renewal, the first paraid is used for subsequent renewals.
+
+Schemas for several of the most common source tables are listed below -- a full list of schemas can be found [here](/schemas).
 
 ### chains.json
 
@@ -174,19 +194,6 @@ block_time              | bigint                | TIMESTAMP |
 block_number            | bigint                | INTEGER                  |
 block_hash              | hex_string            | STRING |
 
-### logs.json
-
-* [logs.json BigQuery schema](https://github.com/colorfulnotion/substrate-etl/tree/main/schema/logs.json)
-
-Field                   | Type                  | BigQuery Type                 |
-------------------------|-----------------------|-------------------------------|
-log_id                  | string                | STRING |
-hash                    | hex_string            | STRING |
-block_time              | bigint                | TIMESTAMP                  |
-block_number            | bigint                | INTEGER                  |
-block_hash              | hex_string            | STRING |
-log                     | JSON                  | JSON |
-
 ### transfers.json
 
 * [transfers.json BigQuery schema](https://github.com/colorfulnotion/substrate-etl/tree/main/schema/transfers.json)
@@ -237,17 +244,6 @@ frozen                  | float                 | FLOAT |
 frozen_usd              | float                 | FLOAT |
 
 
-### specversions.json
-
-* [specversions.json BigQuery Schema](https://github.com/colorfulnotion/substrate-etl/tree/main/schema/specversions.json)
-
-Field                   | Type                  | BigQuery Type         |
-------------------------|-----------------------|-----------------------|
-spec_version            | bigint                | INTEGER               |
-block_number            | bigint                | INTEGER |
-block_hash              | hex_string            | STRING |
-block_time              | bigint                | TIMESTAMP |
-spec                    | JSON                  | JSON  |
 
 ### xcmtransfers.json
 
@@ -304,156 +300,6 @@ See [this blog post](https://colorfulnotion.medium.com/polkaholic-ios-2022-xcm-t
 for a tutorial with representative queries covering the above.
 
 
-### Command Reference
-
-- [chains](#chains)
-- [export](#export)
-- [xcmtransfers](#xcmtransfer)
-
-All the commands accept `-h` parameter for help, e.g.:
-
-```bash
-# ./substrateetl --help
-Usage: substrateetl [options] [command]
-
-Substrate ETL CLI for Polkadot + Kusama Network Substrate chains (C) 2023 Colorful Notion, Inc.
-
-Options:
-  -V, --version                       output the version number
-  -h, --help                          display help for command
-
-Commands:
-  chains [options]                    Dump all chains of relaychain
-  export [options]                    Export chain data
-  xcmtransfers [options]              Export xcmtransfers data
-  help [command]                      display help for command
-```
-
-
-For all `output` parameters the default supported type is json, resulting in newline separated JSON data.  The format type is inferred from the output file name (.json).
-
-#### chains
-
-```bash
-# ./substrateetl chains --help
-Usage: substrateetl chains [options]
-
-Dump all chains of relaychain
-
-Options:
-  -r, --relay-chain <relaychain>  relay chain (polkadot or kusama) (default: "polkadot")
-  -h, --help                      display help for command
-```
-
-Example:
-```bash
-# ./substrateetl chains -r polkadot
-{"para_id":0,"id":"polkadot","chain_name":"Polkadot","ss58_prefix":0,"symbol":"DOT"}
-{"para_id":1000,"id":"statemint","chain_name":"Statemint","ss58_prefix":0,"symbol":"DOT"}
-{"para_id":2000,"id":"acala","chain_name":"Acala","ss58_prefix":10,"symbol":"ACA"}
-{"para_id":2002,"id":"clover","chain_name":"Clover","ss58_prefix":128,"symbol":"CLV"}
-{"para_id":2004,"id":"moonbeam","chain_name":"Moonbeam","ss58_prefix":1284,"symbol":"GLMR"}
-{"para_id":2006,"id":"astar","chain_name":"Astar","ss58_prefix":5,"symbol":"ASTR"}
-...
-{"para_id":2052,"id":"kylin","chain_name":"Kylin","ss58_prefix":42,"symbol":"KYL"}
-```
-
-#### export
-
-Exports block chain data for the relay chain or a specific paraid.
-
-```bash
-# ./substrateetl export --help
-Usage: substrateetl export [options]
-
-Export chain data
-
-Options:
-  -r, --relay-chain <relaychain>  relay chain (polkadot or kusama) (default: "polkadot")
-  -p, --para-id <paraid>          The paraid (if not supplied paraid=0 is the relaychain) (default: "0")
-  -s, --start-date <startdate>    Start date (default: "2022-12-31")
-  -e, --end-date <enddate>        End date (default: "2022-12-31")
-  --start-block <startblock>      Start block
-  --end-block <endblock>          End block
-  --blocks-output <output>        The output file for blocks. If not provided blocks data will not be exported.
-  --extrinsics-output <output>    The output file for extrinsics. If not provided extrinsics data will not be exported.
-  --events-output <output>        The output file for events. If not provided events data will not be exported.
-  --logs-output <output>          The output file for logs. If not provided logs data will not be exported.
-  --transfers-output <output>     The output file for logs. If not provided transfers data will not be exported.
-  --specversions-output <output>  The output file for logs. If not provided spec versions data will not be exported.
-  -h, --help                      display help for command
-```
-
-Example:
-```bash
-./substrateetl export \
-  -r polkadot -p 2012 \
-  --blocks-output blocks.json \
-  --extrinsics-output extrinsics.json \
-  --events-output events.json \
-  --logs-output logs.json \
-  --transfers-output transfers.json \
-  --specversions-output specversions.json
-```
-
-Omit `--blocks-output`, `--extrinsics-output`, `--events-output`, etc. options if you want to export particular sets of data.
-
-#### xcmtransfers
-
-Exports XCM transfers for a relay chain network for a date range.
-
-```
-# ./substrateetl xcmtransfers -h
-Usage: substrateetl xcmtransfers [options]
-
-Export xcmtransfers data
-
-Options:
-  -r, --relay-chain <relaychain>  relay chain (polkadot or kusama) (default: "polkadot")
-  -s, --start-date <startdate>    Start date (default: "2022-12-31")
-  -e, --end-date <enddate>        End date (default: "2022-12-31")
-  --xcmtransfers-output <output>  The output file for xcmtransfers. If not provided xcmtransfers data will not be exported. (default: "xcmtransfers.json")
-  -h, --help                      display help for command
-```
-
-Example:
-```bash
-# ./substrateetl xcmtransfers \
-  -r polkadot \
-  --start-date 2022-12-25 --end-date 2022-12-31 \
-  --xcmtransfers-output xcmtransfers.json 
-```
-
-
-### Public Datasets in BigQuery
-
-Substrate data for each chain is held in 7 tables in one of 2 public
-datasets, with one dataset for each relay chain and all its
-parachains.  By convention, relaychain data is considered "paraid=0".
-
-Project: (Location: us-central1)
-* `substrate-etl`
-
-Datasets: 
-* `polkadot`
-* `kusama`
-
-Tables: (replace `{paraid}` with a specific paraid, e.g. `2000` for `acala`)
-* `blocks{paraid}`
-* `extrinsics{paraid}`
-* `transfers{paraid}`
-* `events{paraid}`
-* `logs{paraid}`
-* `specversions{paraid}`
-* `xcmtransfers` (system)
-
-Thus polkadot relay chain blocks are held in `substrate-etl.polkadot.blocks0`, acala blocks are stored in `substrate-etl.polkadot.blocks2000`, and so on.  
-
-Notes:
-* System tables `chains` and `xcmtransfers` are not specific to any parachain and apply to the whole network.
-* All tables are date partitioned, to support low cost, high speed scans, with the exception of `chains` and `specversions{paraid}`
-* If a parachain has a renewal, the first paraid is used for subsequent renewals.
-
 ### Reporting / Missing data
 
 Generally the data is complete as can be, but the sole data source is the Polkaholic.io indexer.  
@@ -477,11 +323,10 @@ See the **Crawling Status** column for chains with systemic issues, and the **# 
 * All temporal BigQuery datasets are date-partitioned and split into multiple tables by {paraId} to enable low-cost low-latency BigQuery scans for specific date, parachain combinations. Timestamped data use BigQuery TIMESTAMP date types.
 * Addresses are provided in “public key” (signer_pub_key) and SS58 Address (signer_ss58) form to support multi-chain queries with wild card table selection eg`select * from polkadot.extrinsics* where signer_pub_key='<pubkey>' ` aggregates multi-chain transactional history for a given account. 
 * When assets are mentioned (transfers, xcmtransfers), we “decimalize” the output and include basic USD price valuation if possible.
-* EVM Transaction / Transfers support is under active development 
+* EVM Transaction / Transfers support is under active development and is planned for Q2. 
 
 ### Contributions
 
 Contributions are welcome.  Contributors will be invited to a
-dedicated Telegram group and are held to the [Polkadot communities'
-Code of Conduct](https://github.com/paritytech/polkadot/blob/master/CODE_OF_CONDUCT.md).
+dedicated Telegram group and are held to the [Polkadot communities' Code of Conduct](https://github.com/paritytech/polkadot/blob/master/CODE_OF_CONDUCT.md).
 
--- reporter.js
+++ reporter.js
@@ -4,13 +4,28 @@ const path = require("path");
 const fs = require('fs');
 
 const NL = "\r\n";
+const dir = ".";
 
 function currencyFormat(c, _maximumFractionDigits = 2) {
+    try {
+        if (c < .01) _maximumFractionDigits = 5;
+        if (c < .001) _maximumFractionDigits = 43;
+        var formatter = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            maximumFractionDigits: _maximumFractionDigits
+        });
+        return formatter.format(c);
+    } catch (e) {
+        return '-'
+    }
+}
+
+function tokenCount(c, _maximumFractionDigits = 2) {
     try {
         if (c < .01) _maximumFractionDigits = 3;
         if (c < .001) _maximumFractionDigits = 43;
         var formatter = new Intl.NumberFormat('en-US', {
-            style: 'currency',
             currency: 'USD',
             maximumFractionDigits: _maximumFractionDigits
         });
@@ -86,7 +101,7 @@ module.exports = class Reporter {
 	    let numBlocks_missing = rc.numBlocks_missing ? rc.numBlocks_missing.toLocaleString('en-US') : "";
 	    summary.push(`| [${rc.relayChain}](${rc.relayChain}) | ${rc.endDT} | ${rc.numChains} | ${numChains_missing} | ${numBlocks_total} | ${numBlocks_missing} |`);
 	    if ( rc.missing ) {
-		o[relayChain].push(`\r\n## Missing chains\r\n\r\n`);
+		o[relayChain].push(`\r\nMissing chains\r\n\r\n`);
 		if ( numChains_missing > 0 ) {
 		    for (const c of rc.missing) {
 			o[relayChain].push(`* *${c.chainName}* Para ID ${c.paraID}; ${c.crawlingStatus}`);
@@ -95,11 +110,36 @@ module.exports = class Reporter {
 		    o[relayChain].push(`None`);
 		}
 	    }
-	    o[relayChain].push(`\r\nReport source: [${url}](${url}) | See [Definitions](/DEFINITIONS.md) for details`);
 
+	    let assets = rc.assets;
+	    if ( assets && assets.length > 0 ) {
+		let logDT = assets[0].logDT;
+		o[relayChain].push(`# Assets (as of ${logDT})\r\n\r\nSource: [Polkaholic.io](https://polkaholic.io)\r\n\r\n`);
+		o[relayChain].push(`| Symbol | # Holders | # Chains | Free | Reserved | Misc Frozen | Frozen | Recent Price |`);
+		o[relayChain].push(`| ------ | --------- | -------- | ---- | -------- | ----------- | ------ | ------------ |`);
+		for ( const l of assets ) {
+		    let numHolders = l.numHolders.toLocaleString('en-US');
+		    let numChains = l.numChains.toLocaleString('en-US');
+		    let free = l.free > 0 ? tokenCount(l.free) : 0;
+		    let freeUSD = l.freeUSD > 0 ? currencyFormat(l.freeUSD) : "";
+		    let reserved = l.reserved > 0 ? tokenCount(l.reserved) : 0;
+		    let reservedUSD = l.reservedUSD > 0 ? currencyFormat(l.reservedUSD) : "";
+		    let miscFrozen = l.miscFrozen > 0 ? tokenCount(l.miscFrozen) : 0;
+		    let miscFrozenUSD = l.miscFrozenUSD > 0 ? l.miscFrozenUSD : "";
+		    let frozen = l.frozen > 0 ? tokenCount(l.frozen) : 0;
+		    let frozenUSD = l.frozenUSD > 0 ? l.frozenUSD : "";
+		    let priceUSD = l.priceUSD > 0 ? currencyFormat(l.priceUSD) : "";
+		    o[relayChain].push(`| [${l.symbol}](/${rc.relayChain}/assets/${l.symbol}) | ${numHolders} | ${numChains} | ${free} ${freeUSD} | ${reserved} ${reservedUSD} | ${miscFrozen} ${miscFrozenUSD} | | ${frozen} ${frozenUSD} | ${priceUSD} |`);
+		    
+		    await this.asset_report_summary(relayChain, l.symbol, l.url);
+		}
+	    }
+	    o[relayChain].push(`\r\nReport source: [${url}](${url}) | See [Definitions](/DEFINITIONS.md) for details`);
 	}
 	summary.push(`\r\nReport source: [${url}](${url}) | See [Definitions](/DEFINITIONS.md) for details\r\n\r\n`);
 
+	
+	
 	summary.push(`# Dotsama Daily Log (2023)\r\n\r\nSource: [Polkaholic.io](https://polkaholic.io)\r\n\r\n`);
 	summary.push(`| Date            | # Addresses | # Active Accounts | # New Accounts | # Reaped Accounts |`);
 	summary.push(`| ---------------- | ----------- | ----------------- | -------------- | ----------------- |`);
@@ -132,7 +172,62 @@ module.exports = class Reporter {
 	    }
 	}
     }
-
+    
+    async asset_report_summary(relayChain, symbol, url) {
+	// fetch chains where symbol appears
+	let cmd = `curl --silent -H "Content-Type: application/json" --max-time 10 --connect-timeout 5 ${url}`
+        const {
+            stdout,
+            stderr
+        } = await exec(cmd, {
+            maxBuffer: 1024 * 64000
+        });
+	try {
+	    let assetdata = JSON.parse(stdout);
+	    // compile chains using asset
+            let desc = `# ${symbol} substrate-etl Summary (on ${relayChain})\r\n\r\n_Source_: [polkaholic.io](https://polkaholic.io)\r\n\r\n*Relay Chain*: ${relayChain}\r\n\r\n`;
+	    let j = [];
+            j.push(desc);
+            j.push(`| Chain | Asset | # Holders | Free | Reserved | Misc Frozen | Frozen | Price |`);
+            j.push(`| ----- | ----- | --------- | ---- | -------- | ----------- | ------ | ----- |`);
+	    let chains = assetdata.chains;
+	    if ( chains.length > 0 ) {
+		for ( const c of chains ) {
+		    let url = `/${relayChain}/${c.paraID}-${c.id}`
+		    let asset = c.asset ? c.asset : "";
+		    let numHolders = c.numHolders > 0 ? c.numHolders.toLocaleString('en-US') : ""
+		    let free = c.free > 0 ? tokenCount(c.free) : ""
+		    let freeUSD = c.freeUSD > 0 ? currencyFormat(c.freeUSD) : ""
+		    let reserved = c.reserved > 0 ? tokenCount(c.reserved) : ""
+		    let reservedUSD = c.reservedUSD > 0 ? currencyFormat(c.reservedUSD) : ""
+		    let miscFrozen = c.miscFrozen > 0 ? tokenCount(c.miscFrozen) : ""
+		    let miscFrozenUSD = c.miscFrozenUSD > 0 ? currencyFormat(c.miscFrozenUSD) : ""
+		    let frozen = c.frozen > 0 ? tokenCount(c.frozen) : ""
+		    let frozenUSD = c.frozenUSD > 0 ? currencyFormat(c.frozenUSD) : ""
+		    let priceUSD = c.priceUSD > 0 ? currencyFormat(c.priceUSD) : ""
+		    j.push(`| [${c.chainName}](${url}) | \`${asset}\` | ${numHolders} | ${free} ${freeUSD} | ${reserved} ${reservedUSD} | ${miscFrozen}  ${miscFrozenUSD} | ${frozen} ${frozenUSD} | ${priceUSD} |`)
+		}
+		
+		let docs = [];
+		let subdir = path.join(dir, relayChain, `assets`, symbol);
+		if (!fs.existsSync(subdir)) {
+		    fs.mkdirSync(subdir, {
+			recursive: true
+		    });
+		}
+		j.push(`\r\nReport source: [${url}](${url}) | See [Definitions](/DEFINITIONS.md) for details`);
+		let fn_chain = path.join(subdir, `README.md`);
+		let f = fs.openSync(fn_chain, 'w', 0o666);
+		console.log("writing", fn_chain);
+		fs.writeSync(f, j.join(NL) + NL);
+	    } else {
+		// delete it?
+	    }
+	} catch (err) {
+	    console.log("asset_report_summary ERROR fetching", url);
+	}
+    }
+    
     async chain_report_summary(chainID, chain) {
 	// fetch monthly + daily summary for chainID
         let paraID = chain.paraID;
@@ -158,10 +253,25 @@ module.exports = class Reporter {
         j.push(`| Month | Start Block | End Block | # Blocks | # Signed Extrinsics (total) | # Active Accounts (avg) | # Addresses with Balances (max) | Issues |`);
         j.push(`| ----- | ----------- | --------- | -------- | --------------------------- | ----------------------- | ------------------------------- | ------ |`);
         docs = [];
-        docs.push(`\r\n## # Blocks\r\n\`\`\`\r\nSELECT LAST_DAY( date(block_time)) as monthDT, Min(date(block_time)) startBN, max(date(block_time)) endBN, min(number) minBN, max(number) maxBN, count(*) numBlocks, max(number)-min(number)+1-count(*) as numBlocks_missing FROM \`substrate-etl.${relayChain}.blocks${paraID}\` group by monthDT order by monthDT desc\r\n\`\`\`\r\n\r\n`);
+
+	docs.push(`\r\n## substrate-etl Tables:\r\n`)
+	docs.push(`* _Blocks_: \`substrate-etl.${relayChain}.blocks${paraID}\` (date-partitioned by \`block_time\`) - [Schema](/schema/balances.json)`)
+	docs.push(`* _Extrinsics_: \`substrate-etl.${relayChain}.extrinsics${paraID}\` (date-partitioned by \`block_time\`) - [Schema](/schema/extrinsics.json)`)
+	docs.push(`* _Events_: \`substrate-etl.${relayChain}.events${paraID}\` (date-partitioned by \`block_time\`) - [Schema](/schema/events.json)`)
+	docs.push(`* _Transfers_: \`substrate-etl.${relayChain}.transfers${paraID}\` (date-partitioned by \`block_time\`) - [Schema](/schema/transfers.json)`)
+	docs.push(`* _Balances_: \`substrate-etl.${relayChain}.balances${paraID}\` (date-partitioned by \`ts\`) - [Schema](/schema/balances.json)`)
+	docs.push(`* _Active Accounts_: \`substrate-etl.${relayChain}.accountsactive${paraID}\` (date-partitioned by \`ts\`) - [Schema](/schema/accountsactive.json)`)
+	docs.push(`* _Passive Accounts_: \`substrate-etl.${relayChain}.accountspassive${paraID}\` (date-partitioned by \`ts\`) - [Schema](/schema/accountspassive.json)`)
+	docs.push(`* _New Accounts_: \`substrate-etl.${relayChain}.accountsnew${paraID}\` (date-partitioned by \`ts\`)  - [Schema](/schema/accountsnew.json)`)
+	docs.push(`* _Reaped Accounts_: \`substrate-etl.${relayChain}.accountsreaped${paraID}\` (date-partitioned by \`ts\`) - [Schema](/schema/accountsreaped.json)`)
+	docs.push(`* _Assets_: \`substrate-etl.${relayChain}.assets\` (filter on \`${paraID}\`) - [Schema](/schema/assets.json)`)
+	docs.push(`* _XCM Assets_: \`substrate-etl.${relayChain}.xcmassets\` (filter on \`para_id\`) - [Schema](/schema/xcmassets.json)`)
+	docs.push(`* _XCM Transfers_: \`substrate-etl.${relayChain}.xcmtransfers\` (filter on \`origination_para_id\` or \`destination_para_id\`, date-partitioned by \`origination_ts\`) - [Schema](/schema/xcmtransfers.json)`)
+
+        docs.push(`\r\n### # Blocks\r\n\`\`\`\r\nSELECT LAST_DAY( date(block_time)) as monthDT, Min(date(block_time)) startBN, max(date(block_time)) endBN, min(number) minBN, max(number) maxBN, count(*) numBlocks, max(number)-min(number)+1-count(*) as numBlocks_missing FROM \`substrate-etl.${relayChain}.blocks${paraID}\` group by monthDT order by monthDT desc\r\n\`\`\`\r\n\r\n`);
 	let broken = [];
 
-        let prevStartBN = null;
+	let prevStartBN = null;
         for (const r of chaindata.monthly) {
             let monthDT = r.monthDT ? r.monthDT : "";
             let startDT = r.startDT ? r.startDT : "";
@@ -189,8 +299,37 @@ module.exports = class Reporter {
             let url = `/${relayChain}/${paraID}-${id}/${monthDT}.md`
             j.push(`| [${desc}](${url}) | ${startBN} | ${endBN} | ${numBlocks_total} | ${numSignedExtrinsics} | ${numActiveAccounts} | ${numAddresses} | ${issues} ${numBlocks_missing} ${percent_missing} |   `)
         }
+
+	if ( chaindata.assets && chaindata.assets.length ) {
+            let desc = `\r\n## ${chain.chainName} Assets\r\n`;
+            j.push(desc);
+            j.push(`\r\n\r\n| Symbol | # Holders | Free | Reserved | Misc Frozen | Frozen | Price | AssetID | `);
+            j.push(`| ----- | --------- | ---- | -------- | ----------- | ------ | ----- | --- |`);
+	    let assets = chaindata.assets;
+		
+            for (const c of chaindata.assets) {
+		let symbol = c.symbol ? c.symbol : "";
+		if ( symbol ) {
+		    let url = `/${relayChain}/assets/${symbol}`
+		    let asset = c.asset ? c.asset : "";
+		    let numHolders = c.numHolders > 0 ? c.numHolders.toLocaleString('en-US') : ""
+		    let free = c.free > 0 ? tokenCount(c.free) : ""
+		    let freeUSD = c.freeUSD > 0 ? currencyFormat(c.freeUSD) : ""
+		    let reserved = c.reserved > 0 ? tokenCount(c.reserved) : ""
+		    let reservedUSD = c.reservedUSD > 0 ? currencyFormat(c.reservedUSD) : ""
+		    let miscFrozen = c.miscFrozen > 0 ? tokenCount(c.miscFrozen) : ""
+		    let miscFrozenUSD = c.miscFrozenUSD > 0 ? currencyFormat(c.miscFrozenUSD) : ""
+		    let frozen = c.frozen > 0 ? tokenCount(c.frozen) : ""
+		    let frozenUSD = c.frozenUSD > 0 ? currencyFormat(c.frozenUSD) : ""
+		    let priceUSD = c.priceUSD > 0 ? currencyFormat(c.priceUSD) : ""
+		    j.push(`| [${symbol}](${url}) | ${numHolders} | ${free} ${freeUSD} | ${reserved} ${reservedUSD} | ${miscFrozen}  ${miscFrozenUSD} | ${frozen} ${frozenUSD} | ${priceUSD} |   \`${asset}\` | `)
+		}
+            }
+
+	}
+
+	    
 	docs.push(`\r\nReport source: [${url}](${url}) | See [Definitions](/DEFINITIONS.md) for details`);
-	let dir = ".";
         j = j.concat(docs);
 
 
